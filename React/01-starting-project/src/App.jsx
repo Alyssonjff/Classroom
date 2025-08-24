ed6211d4445cae12db8a18e683e4e3f6578f066c
Alyssonjff <alyssonjfranca@gmail.com>
2025-08-24 12:30:28 -0300

    create components folder

--- React/01-starting-project/src/App.jsx
+++ React/01-starting-project/src/App.jsx
@@ -1,34 +1,7 @@
-import reactImg from './assets/react-core-concepts.png';
 import { CORE_CONCEPTS } from './data';
+import Header from './components/Header';
+import CoreConcepts from './components/CoreConcept';
 
-const reactDescription = ['Fundamental','Crucial', 'Core'];
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const descripton = reactDescription[genRandomInt(2)];
-  return (
-      <header>
-        <img src={reactImg} alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          {descripton} React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-      );
-}
-
-function CoreConcepts({image, title, description}) {
-  return (
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  );
-}
 
 function App() {
   return (
